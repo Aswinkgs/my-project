629e61c8165cdc8dc27bd7db3c5e4da2530b9185
vishnu <ashwinvishnu123@gmail.com>
2026-09-12 10:35:35 +0530

    aws s3

--- terraform/ptovider.tf
+++ terraform/ptovider.tf
@@ -10,3 +10,11 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "aswinkgs-flask-tfstate"
+    key    = "flask-app/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
